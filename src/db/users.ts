b2356b830b27fa600e12e6b0de1d33423a42bb44
M. Andres Jurado <anmijurane@gmail.com>
2025-11-19 21:26:50 -0600

    ADD:  new user

--- src/db/users.ts
+++ src/db/users.ts
@@ -2,6 +2,7 @@ export const usersAllowed = [
   { name: 'fpt_ops_21c2', secret: '$2b$10$AgvLjgn2748SmxzcPlwD/ONteNG6pycF8PhxmazeTPbBciVtLMx.G', role: '006' },
   { name: 'fpt_mkt_c3b9', secret: '$2b$10$2.42Bxui4fWjr8P.oap9LObDbBwLHtFUxktW7w7Fat5H1OGhQzaXW', role: '006' },
   { name: 'fpt_tic_a444', secret: '$2b$10$x4oAJ3Yl/YJFuclOGZBOI.Y0HKmc30642VM7VeXXqB7G8oKEh3aIG', role: '006' },
+  { name: 'fpt_tic_683d', secret: '$2b$10$3wbMAbuAnu29zpQmn1TM0u8hmFk0j.PKpG9AysGdBSBX9yZhsyTpy', role: '006' },
   { name: 'stl_gen_e95c', secret: '$2b$10$hNji0XmtrRrK960pVLXvWOrvv3qb5ao1FzrYoQCITM2NIAHKnIXe2', role: '006' },
   { name: 'stl_dot_e95c', secret: '$2b$10$Bivs8L4So3rxtC6noTOO9.bRg8aaJu0cZb0TtfjzQheOT9mXgmaCu', role: '465' },
   { name: 'stl_anm_92f1', secret: '$2b$10$DT7ORCFgq2sO8Xjg.2Nh2Owjdogr56rxPpEIgZUVS7fpu8vcc3bQa', role: '465' },
