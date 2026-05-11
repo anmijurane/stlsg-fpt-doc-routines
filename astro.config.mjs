65455cd1bd7fe29cf4389823eaed3ae273ebf079
Andres Jurado anmijurane <anmijurane@gmail.com>
2026-05-11 11:01:07 -0600

    Fix: breaking changes of upgrade deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,15 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import dotenv from 'dotenv';
-
 import node from '@astrojs/node';
-
-import tailwindcss from '@tailwindcss/vite';
 import starlightOpenAPIPlugin, { openAPISidebarGroups } from 'starlight-openapi';
+import tailwindcss from '@tailwindcss/vite';
 
 dotenv.config();
 
 const PORT = Number(process.env.PORT) || 8765;
 
 console.log('openAPISidebarGroups', openAPISidebarGroups);
-
 
 export default defineConfig({
   site: 'https://fpt-routines.sitelseg.mx/',
@@ -22,10 +19,6 @@ export default defineConfig({
   }),
 
   output: 'server',
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
 
   server: {
     port: PORT
@@ -36,15 +29,17 @@ export default defineConfig({
       disable404Route: true,
       prerender: false,
       title: 'Planet Fitness',
-      plugins: [starlightOpenAPIPlugin([
-        {
-          base: 'api-docs',
-          schema: './src/swagger/swagger_fpt_api_analytics.yml',
-          sidebar: {
-            label: 'API Analítica'
+      plugins: [
+        starlightOpenAPIPlugin([
+          {
+            base: 'api-docs',
+            schema: './src/swagger/swagger_fpt_api_analytics.yml',
+            sidebar: {
+              label: 'API Analítica'
+            }
           }
-        }
-      ])],
+        ])
+      ],
       logo: {
         src: './src/assets/pf_logo.webp',
       },
@@ -59,52 +54,53 @@ export default defineConfig({
         },
         {
           label: 'Discovery',
-          autogenerate: { directory: 'discovery' },
+          items: [{ autogenerate: { directory: 'discovery' } }],
         },
         {
           label: 'Historias de usuario',
           items: [
             { label: 'Vista general', slug: 'user-stories/structure' },
-            { label: 'Epic 1: Acceso e identificación de Club', autogenerate: { directory: 'user-stories/mod01' }, collapsed: true, },
-            { label: 'Epic 2: Exploracion de rutinas', autogenerate: { directory: 'user-stories/mod02' }, collapsed: true, },
-            { label: 'Epic 3: Pantalla informativa', autogenerate: { directory: 'user-stories/mod03' }, collapsed: true, },
-            { label: 'Epic 4: Vista de rutinas', autogenerate: { directory: 'user-stories/mod04' }, collapsed: true, },
-            { label: 'Epic 5: Detalle de ejercicio', autogenerate: { directory: 'user-stories/mod05' }, collapsed: true, },
-            { label: 'Epic 6: Reproducción de video', autogenerate: { directory: 'user-stories/mod06' }, collapsed: true, },
-            { label: 'Epic 7: Marcado local de progreso', autogenerate: { directory: 'user-stories/mod07' }, collapsed: true, },
-            { label: 'Epic 8: Retroalimentación', autogenerate: { directory: 'user-stories/mod08' }, collapsed: true, },
-            { label: 'Epic 9: Disclaimer Legal de Uso', autogenerate: { directory: 'user-stories/mod09' }, collapsed: true, },
+            { label: 'Epic 1: Acceso e identificación de Club', collapsed: true, items: [{ autogenerate: { directory: 'user-stories/mod01', collapsed: true } }] },
+            { label: 'Epic 2: Exploracion de rutinas', collapsed: true, items: [{ autogenerate: { directory: 'user-stories/mod02', collapsed: true } }] },
+            { label: 'Epic 3: Pantalla informativa', collapsed: true, items: [{ autogenerate: { directory: 'user-stories/mod03', collapsed: true } }] },
+            { label: 'Epic 4: Vista de rutinas', collapsed: true, items: [{ autogenerate: { directory: 'user-stories/mod04', collapsed: true } }] },
+            { label: 'Epic 5: Detalle de ejercicio', collapsed: true, items: [{ autogenerate: { directory: 'user-stories/mod05', collapsed: true } }] },
+            { label: 'Epic 6: Reproducción de video', collapsed: true, items: [{ autogenerate: { directory: 'user-stories/mod06', collapsed: true } }] },
+            { label: 'Epic 7: Marcado local de progreso', collapsed: true, items: [{ autogenerate: { directory: 'user-stories/mod07', collapsed: true } }] },
+            { label: 'Epic 8: Retroalimentación', collapsed: true, items: [{ autogenerate: { directory: 'user-stories/mod08', collapsed: true } }] },
+            { label: 'Epic 9: Disclaimer Legal de Uso', collapsed: true, items: [{ autogenerate: { directory: 'user-stories/mod09', collapsed: true } }] },
+            { label: 'Epic 10: Perfilamiento Demográfico de Usuario', collapsed: true, items: [{ autogenerate: { directory: 'user-stories/mod10', collapsed: true } }] },
           ],
         },
         {
           label: 'Planeacion',
-          autogenerate: { directory: 'planning' },
-          collapsed: true
+          collapsed: true,
+          items: [{ autogenerate: { directory: 'planning', collapsed: true } }]
         },
         {
           label: 'Diseño',
           items: [
-            { label: 'Categorias de rutinas', autogenerate: { directory: 'design/routines-category' }, collapsed: true },
-            { label: 'Disclamer', autogenerate: { directory: 'design/disclamer' }, collapsed: true },
-            { label: 'Nivel de rutina', autogenerate: { directory: 'design/routine-level' }, collapsed: true },
-            { label: 'Detalle del ejercicio', autogenerate: { directory: 'design/exercise-detail' }, collapsed: true },
-            { label: 'Video ejecución', autogenerate: { directory: 'design/video-exercise' }, collapsed: true },
+            { label: 'Categorias de rutinas', collapsed: true, items: [{ autogenerate: { directory: 'design/routines-category', collapsed: true } }] },
+            { label: 'Disclamer', collapsed: true, items: [{ autogenerate: { directory: 'design/disclamer', collapsed: true } }] },
+            { label: 'Nivel de rutina', collapsed: true, items: [{ autogenerate: { directory: 'design/routine-level', collapsed: true } }] },
+            { label: 'Detalle del ejercicio', collapsed: true, items: [{ autogenerate: { directory: 'design/exercise-detail', collapsed: true } }] },
+            { label: 'Video ejecución', collapsed: true, items: [{ autogenerate: { directory: 'design/video-exercise', collapsed: true } }] },
           ],
           collapsed: true
         },
         {
           label: 'Datos de rutinas',
           items: [
-            { label: 'Concentrado', autogenerate: { directory: 'data-information/concentrate', }, collapsed: true },
-            { label: 'Accesos QR', autogenerate: { directory: 'data-information/QR', }, collapsed: true },
+            { label: 'Concentrado', collapsed: true, items: [{ autogenerate: { directory: 'data-information/concentrate', collapsed: true } }] },
+            { label: 'Accesos QR', collapsed: true, items: [{ autogenerate: { directory: 'data-information/QR', collapsed: true } }] },
             { label: 'Listado de gimnasios', slug: 'data-information/gym-list', },
           ],
           collapsed: true
         },
         {
           label: 'Feedback de rutinas',
-          autogenerate: { directory: 'feedback/periods' },
-          collapsed: true
+          collapsed: true,
+          items: [{ autogenerate: { directory: 'feedback/periods', collapsed: true } }]
         },
         {
           label: 'API Diccionario',
@@ -125,5 +121,10 @@ export default defineConfig({
       },
     }),
   ],
-
+  vite: {
+    plugins: [
+      // @ts-ignore
+      tailwindcss()
+    ],
+  },
 });
